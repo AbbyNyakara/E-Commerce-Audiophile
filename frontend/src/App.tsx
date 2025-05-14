e926b70d72788e13dc3e8fb89b08d664096c3674
Abigael Nyakara <mogusuab@msu.edu>
2025-05-14 15:52:24 -0400

    add gear

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import HeadPhones from './components/products/headphones/headphones'
 import Footer from './components/common/footer/Footer'
+import Gear from './components/common/audio_gear/Gear'
 
 const App = () => {
   return (
     <div>
       <HeadPhones />
+      <Gear />
       <Footer />
     </div>
   )
