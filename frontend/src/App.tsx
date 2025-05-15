9bcd5587ca535724de53515507f46e7f167d71f9
Abigael Nyakara <mogusuab@msu.edu>
2025-05-15 12:38:28 -0400

    add Header and navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import './App.css'
 import HeadPhones from './components/products/headphones/headphones'
 import Footer from './components/common/footer/Footer'
 import Gear from './components/common/audio_gear/Gear'
+import Header from './components/common/header/Header'
 
 const App = () => {
   return (
     <div>
+      <Header />
       <HeadPhones />
       <Gear />
       <Footer />
---
+++ frontend/src/components/common/header/Header.tsx
@@ -0,0 +1,33 @@
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <header className={styles.navbar}>
+      <div className={styles.nav_logo}>
+        <a href='#'>
+          <img src='./assets/shared/desktop/logo.svg' />
+        </a>
+      </div>
+
+      <ul className={styles.navlinks}>
+        <li>
+          <a href='#'>home</a>
+        </li>
+        <li>
+          <a href='#'>headphones</a>
+        </li>
+        <li>
+          <a href='#'>speakers</a>
+        </li>
+        <li>
+          <a href='#'>earphones</a>
+        </li>
+      </ul>
+      <div className={styles.cart_section}>
+        <img src='./assets/shared/desktop/icon-cart.svg' alt='Cart' />
+      </div>
+    </header>
+  )
+}
+
+export default Header
