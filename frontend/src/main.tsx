c4a82c294551711301e994547ae3d18bf7077435
Abigael Nyakara <mogusuab@msu.edu>
2025-05-19 15:36:57 -0400

    add code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/Homepage/Homepage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <div>404 Not Found</div>, // create a not found component with link to the
+  },
+  {
+    path: '/app',
+    element: <App />,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
